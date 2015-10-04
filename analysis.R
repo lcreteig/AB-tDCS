1acfe31574a03327b18a198a5079b4dbab2a2ad8
Leon Reteig <leonreteig@gmail.com>
2015-10-04 13:05:41 +0200

    Simplified T2 given T1 calculation
    While checking whether code works correctly.
    Verified that it did, and also that this change did not alter the results.

--- analysis.R
+++ analysis.R
@@ -60,7 +60,7 @@ for (iSub in subID) {
         T1 <- sum(trialsT1 & trialsLag) / numTrials * 100 # percentage of T1 correct trials
         
         trialsT2 <- ABdata$T2acc == 1 # indices of T2 correct trials
-        nonBlink <- sum(ABdata$T2acc[trialsLag & trialsT1 & trialsT2]) / numTrials * 100 # percentage of T2 given T1 correct trials
+        nonBlink <- sum(trialsLag & trialsT1 & trialsT2) / numTrials * 100 # percentage of T2 given T1 correct trials
         
         # insert into data frame
         cellIdx = groupData$subject == iSub & groupData$stimulation == stimulation[tDCScode == iStim] & groupData$block == iBlock & groupData$lag == iLag # get index of current cell
