48ace9affe42b809bb7c3e22e0100af8a1b53d1e
leonreteig@gmail.com <leonreteig@gmail.com>
2016-04-22 11:43:16 +0200

    Analysis of T1 and T2 swaps
    Modify MATLAB behavioral analysis script of the pilot data, to also
    plot proportion of trials on which T1 and T2 were swapped, actual T2
    was reported as T1, and actual T1 was reported as T2. Especially
    in the latter case it can be argued that these are also blink(-like)
    trials.

--- behavAnalysis.m
+++ behavAnalysis.m
@@ -1,5 +1,8 @@
+% Quick script to find out whether / how often T1+T2 identities tend to be
+% swapped
+
 %% Setup
-addpath(genpath(fullfile(pwd,'src'))) % add source code for analysis to matlab path
+addpath(fullfile(pwd,'src')) % add source code for analysis to matlab path
 
 %% Load data
 fileInfo = dir([fullfile(pwd,'data', 'pilot') filesep '*.txt']); % get info on text files to load
@@ -19,6 +22,9 @@ for iLag = 1:length(lags)
         trialIdx = data.lag(iSub,:) == lags(iLag) & logical(data.T1acc(iSub,:)); % trials on which T1 was accurately reported for this subject and lag
         T1acc(iSub,iLag) = sum(data.T1acc(iSub,trialIdx)) / trialsPerLag; % calculate proportion T1 correct
         T2ifT1acc(iSub, iLag) = sum(data.T2acc(iSub, trialIdx & logical(data.T2acc(iSub,:)))) / trialsPerLag ; % calculate proportion T2 correct given T1 correct
+        swap(iSub, iLag) = sum(data.swap(iSub, data.lag(iSub,:) == lags(iLag))) / trialsPerLag;
+        T1isT2(iSub, iLag) = sum(data.T1isT2(iSub, data.lag(iSub,:) == lags(iLag))) / trialsPerLag;
+        T2isT1(iSub, iLag) = sum(data.T2isT1(iSub, data.lag(iSub,:) == lags(iLag))) / trialsPerLag;
     end
 end
 
@@ -47,3 +53,42 @@ ylim([0 1])
 set(gca, 'xTick', lags)
 xlabel('lag', 'fontSize', 14)
 ylabel('proportion correct', 'fontSize', 14)
+
+%% Plot proportion T2 and T1 swapped
+
+figure('color', 'w')
+hold on
+title('T1 and T2 swapped', 'fontSize', 16)
+plot(lags,swap, 'color', [0.7 0.7 0.7]) % plot single subject data
+plot(lags,mean(swap)', '-Ok', 'lineWidth', 3) % plot average
+ylim([0 1])
+
+set(gca, 'xTick', lags)
+xlabel('lag', 'fontSize', 14)
+ylabel('proportion of trials', 'fontSize', 14)
+
+%% Plot proportion T2 identity reported as T1
+
+figure('color', 'w')
+hold on
+title('T2 identity reported as T1', 'fontSize', 16)
+plot(lags,T1isT2, 'color', [0.7 0.7 0.7]) % plot single subject data
+plot(lags,mean(T1isT2)', '-Ok', 'lineWidth', 3) % plot average
+ylim([0 1])
+
+set(gca, 'xTick', lags)
+xlabel('lag', 'fontSize', 14)
+ylabel('proportion of trials', 'fontSize', 14)
+
+%% Plot proportion T1 identity reported as T2
+
+figure('color', 'w')
+hold on
+title('T1 identity reported as T2', 'fontSize', 16)
+plot(lags,T2isT1, 'color', [0.7 0.7 0.7]) % plot single subject data
+plot(lags,mean(T2isT1)', '-Ok', 'lineWidth', 3) % plot average
+ylim([0 1])
+
+set(gca, 'xTick', lags)
+xlabel('lag', 'fontSize', 14)
+ylabel('proportion of trials', 'fontSize', 14)
--- src/extractBehavData.m
+++ src/extractBehavData.m
@@ -9,7 +9,7 @@ for iSub=1:length(files)
         %initialize data structure
         data = struct('lag', zeros(length(files),length(ABdat.data)), ...
             'T1acc', zeros(length(files),length(ABdat.data)), 'T2acc', zeros(length(files),length(ABdat.data)), ...
-            'T1isT2', zeros(length(files),length(ABdat.data)), 'T2isT1', zeros(length(files),length(ABdat.data)));
+            'swap', zeros(length(files),length(ABdat.data)), 'T1isT2', zeros(length(files),length(ABdat.data)), 'T2isT1', zeros(length(files),length(ABdat.data)));
         end
         
         data.lag(iSub,:) = str2double(ABdat.textdata(2:end,strcmp('lag', varNames))); % store lag of each trial
@@ -20,15 +20,17 @@ for iSub=1:length(files)
      
      T1real = ABdat.textdata(iTrial, strcmp('T1letter', varNames)); %get actual T1 identity
      T1ans = ABdat.textdata(iTrial, strcmp('T1resp', varNames)); %get T1 response of subject
-     T2real = ABdat.textdata(iTrial, strcmp('T12letter', varNames)); %get actual T2 identity
+     T2real = ABdat.textdata(iTrial, strcmp('T2letter', varNames)); %get actual T2 identity
      T2ans = ABdat.textdata(iTrial, strcmp('T2resp', varNames)); %T2 response of subject
      
-     if strcmp(T1ans, T2real) % If the identity of T2 was reported as T1
-         data.T1isT2(iSub,iTrial) = 1; % mark trial
-     end
-     
-     if strcmp(T2ans, T1real)  % If the identity of T2 was reported as T1
-         data.T2isT1(iSub,iTrial) = 1; % mark trial
+     if strcmp(T1ans, T2real) && strcmp(T2ans, T1real) % if T2 and T1 were swapped
+         data.swap(iSub, iTrial-1) = 1; % mark trial
+         
+     elseif strcmp(T1ans, T2real) % If the identity of T2 was reported as T1
+         data.T1isT2(iSub,iTrial-1) = 1;
+         
+     elseif strcmp(T2ans, T1real)  % If the identity of T1 was reported as T2
+         data.T2isT1(iSub,iTrial-1) = 1;
      end
          
  	end
