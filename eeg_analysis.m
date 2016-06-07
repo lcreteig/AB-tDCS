2fc236cded934928e1d076f8161dc30623c03429
leonreteig@gmail.com <leonreteig@gmail.com>
2016-06-07 12:36:37 +0200

    Plot ERPs after preprocessing
    Added function to make line- and topo plots of ERPs after
    preprocessing, to inspect data quality at this point.

--- eeg_analysis.m
+++ eeg_analysis.m
@@ -1,29 +1,28 @@
-paths.srcDir = fullfile('src', 'eeg', 'preproc'); % directory containing all preprocessing code for the project
-addpath(paths.srcDir);
+clear all, close all
 
+%% Config
 % subjects = {'S01', 'S02', 'S03', 'S04', 'S05', 'S06', 'S07', 'S08', ...
 % 'S09', 'S10','S11', 'S12', 'S13', 'S15', 'S16', 'S17', 'S18', 'S19', 'S20', ...
 % 'S21'};
-% sessions = {'B', 'D'};
-% blocks = {'pre','tDCS','post'};
-subjects = 'S01';
+subjects = 'S02';
 sessions = 'B';
-blocks = 'tDCS';
+%sessions = {'B','D'};
+% blocks = {'pre','tDCS','post'};
+blocks = 'post';
 eeglabVersion = 'eeglab13_5_4b'; % either 'eeglab8_0_3_5b' for MATLAB r2010b or 'eeglab13_5_4b' for r2012b and r2015a.
 sendMail = false;
 
+%% Setup
+paths.srcDir = fullfile('src', 'eeg', 'preproc'); % directory containing all preprocessing code for the project
+addpath(paths.srcDir);
+
 [preproc, paths, trig] = preproc_config(subjects, sessions, blocks, eeglabVersion, sendMail);
+
+%% Preprocess
+
 preprocess;
 
-% TO DO:
+%% Inspect ERPs after preprocessing
 
-% Exclude zero'd channels (and reference?) from ICA?
-% Cut smaller epochs for ICA, mirror and splice after ICA. OR: run ICA on smaller epochs; import weights in larger??
-% plot 35 components
-% SASICA?
-
-% double path somewhere (local Rekenbeest?)
-% Newest eeglab: subtract 61440 from each event
-% Recode triggers before cutting data in tDCS step
-% Make eeglab_version a variable, and let the script behave differently for
-% old and new? (instead of checkVersion function). eeglab13_5_4b works on MATLAB 2015a and 2012b! 
+addpath(fullfile('src','eeg'));
+plotERPs(subjects, sessions, blocks); % N.B.: must be a single subject/session/block
