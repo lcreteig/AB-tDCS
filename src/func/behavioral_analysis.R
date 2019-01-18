fb3b2849f8bb5277a1ee27e9303f33805da583d3
leonreteig@gmail.com <leonreteig@gmail.com>
2019-01-18 16:24:52 +0100

    Move code chunks into functions
    Lots of code chunks were moved to behavioral_analysis.R, such that
    they could also called be called in the Rmarkdown file of the
    manuscript there. To still keep the AB_tDCS-EEG notebook complete,
    the content of these code chunks is still shown there, using
    knitr::read_chunk().

--- src/func/behavioral_analysis.R
+++ src/func/behavioral_analysis.R
@@ -62,11 +62,11 @@ load_data_study2 <- function(dataDir, subs_incomplete) {
           numTrials <- sum(trialsLag) # number of trials of this lag
           
           trialsT1 <- ABdata$T1acc == 1 # indices of T1 correct trials
-          T1 <- sum(trialsT1 & trialsLag) / numTrials * 100 # percentage of T1 correct trials
+          T1 <- sum(trialsT1 & trialsLag) / numTrials # proportion of T1 correct trials
           
           trialsT2 <- ABdata$T2acc == 1 # indices of T2 correct trials
-          T2 <- sum(trialsT2 & trialsLag) / numTrials * 100 # percentage of T1 correct trials
-          nonBlink <- sum(trialsLag & trialsT1 & trialsT2) / sum(trialsLag & trialsT1) * 100 # percentage of T2 given T1 correct trials
+          T2 <- sum(trialsT2 & trialsLag) / numTrials  # proportion of T1 correct trials
+          nonBlink <- sum(trialsLag & trialsT1 & trialsT2) / sum(trialsLag & trialsT1) # proportion of T2 given T1 correct trials
           
           # insert into data frame
           cellIdx = groupData$subject == iSub & groupData$stimulation == stimulation[tDCScode == iStim] & groupData$block == iBlock & groupData$lag == iLag # get index of current cell
@@ -87,4 +87,75 @@ load_data_study2 <- function(dataDir, subs_incomplete) {
   }
   groupData$first.session <- factor(groupData$first.session)
   return(groupData)
+}
+
+## ---- Function to format data from study 1 as in study 2
+format_study2 <- function(df) {
+  df %>%
+    select(fileno, First_Session, ends_with("_NP"), -contains("Min")) %>% # keep only relevant columns
+    gather(key, accuracy, -fileno, -First_Session) %>% # make long form
+    # split key column to create separate columns for each factor
+    separate(key, c("block", "stimulation", "target", "lag"), c(1,3,5)) %>% # split after 1st, 3rd, and 5th character
+    # convert to factors, relabel levels to match those in study 2
+    mutate(block = factor(block, levels = c("v", "t", "n"), labels = c("pre", "tDCS", "post")),
+           stimulation = factor(stimulation, levels = c("b_", "d_"), labels = c("anodal", "cathodal")),
+           lag = factor(lag, levels = c("_2_NP", "_4_NP", "_10_NP"), labels = c(2, 4, 10)),
+           First_Session = factor(First_Session, labels = c("anodal first","cathodal first"))) %>%
+    spread(target, accuracy) %>% # create separate columns for T2.given.T1 and T1 performance
+    rename(T2.given.T1 = NB, session.order = First_Session, subject = fileno)# rename columns to match those in study 2
+}
+
+## ---- Function to create lag 3 in study 1 data
+create_lag3_study1 <- function(df) {
+  df %>%
+  group_by(subject, session.order, block, stimulation) %>% # for each condition
+    # calculate lag 3 as mean of 2 and 4; keep lag 10
+    summarise(lag3_T2.given.T1 = (first(T2.given.T1) + nth(T2.given.T1,2)) / 2,
+              lag3_T1 = (first(T1) + nth(T1,2)) / 2,
+              lag10_T2.given.T1 = last(T2.given.T1), 
+              lag10_T1 = last(T1)) %>%
+    gather(condition, score, -subject, -session.order, -block, -stimulation) %>% # gather the 4 columns we created
+    separate(condition, c("lag", "measure"), "_") %>% # separate lag and T1/T2|T1
+    mutate(lag = str_remove(lag,"lag")) %>%
+    mutate(lag = fct_relevel(lag, "3","10")) %>% #remake the lag factor
+    spread(measure, score)  # make a column of T1 and T2|T1
+}
+
+## ---- Function to calculate AB magnitude
+calc_ABmag <- function(df) {
+  df %>% 
+    group_by(subject, session.order, stimulation, block) %>% # for each unique factor combination
+    summarise(AB.magnitude = last(T2.given.T1) - first(T2.given.T1), # subtract lags to replace data with AB magnitude,
+              T1.short = first(T1)) # also keep T1 performance for short lag, to use as a covariate later
+}
+
+## ---- Function to calculate change scores
+calc_change_scores <- function(df) {
+  df %>%
+    gather(measure, performance, AB.magnitude, T1.short) %>%
+    group_by(subject, session.order, stimulation, measure) %>%
+    summarise(baseline = first(performance), 
+              during = nth(performance,2) - baseline,
+              after = last(performance) - baseline) %>%
+    gather(change, change.score, during, after) %>%
+    mutate(change = fct_recode(change, "tDCS - baseline" = "during", "post - baseline" = "after"),
+           change = fct_relevel(change, "tDCS - baseline")) %>%
+    arrange(subject, stimulation)
+}
+
+## ---- Function to calculate partial correlations of anodal vs cathodal change scores
+pcorr_anodal_cathodal <- function(df) {
+  df %>%
+    # create two columns of AB magnitude change score during tDCS: for anodal and cathodal
+    filter(measure == "AB.magnitude", change == "tDCS - baseline") %>%
+    select(-baseline) %>%
+    spread(stimulation, change.score) %>%
+    ungroup() %>% # remove grouping from previous steps, as we need to modify the dataframe
+    select(session.order, anodal, cathodal) %>% # keep only relevant columns
+    mutate(session.order = as.numeric(session.order)) %>% # dummy code to use as covariate
+    
+    # partial correlation  
+    summarise(r = pcor(c("anodal","cathodal","session.order"), var(.))) %>% # partial correlation coefficient
+    mutate(stats = list(as.data.frame(pcor.test(r, 1, n_distinct(df$subject))))) %>% # t-stat, df and p-value of coefficient
+    unnest(stats) # unpack resulting data frame into separate columns %>%
 }
